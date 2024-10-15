d268b5fae5168caeccae1caf5761a9e880913848
Dominic Baccay <91103278+dlmbaccay@users.noreply.github.com>
2024-10-15 18:27:05 +0800

    main branch fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,8 @@ const RootLayout = () => {
     <PaperProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        
+        {/* TODO: implement */}
         {/* <Stack.Screen name="(auth)" options={{ headerShown: false }} /> */}
         {/* <Stack.Screen name="(home)" options={{ headerShown: false }} /> */}
         {/* <Stack.Screen name="(profile)" options={{ headerShown: false }} /> */}
