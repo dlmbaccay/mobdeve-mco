5ab6a76d0aadd12cd832d485d9a57b73b1d56926
Dominic Baccay <91103278+dlmbaccay@users.noreply.github.com>
2024-10-16 23:17:06 +0800

    revamped layout, restricted to light mode only

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,22 @@
 import { Stack } from "expo-router";
-import { PaperProvider } from "react-native-paper";
+import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
+import { MD3LightTheme, PaperProvider } from 'react-native-paper';
 
 const RootLayout = () => {
+  const { theme } = useMaterial3Theme();
+  const lightTheme = {
+    ...MD3LightTheme,
+    colors: theme.light,
+  };
+
   return (
-    <PaperProvider>
+    <PaperProvider theme={lightTheme}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        
+        <Stack.Screen name="home" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+
         {/* TODO: implement */}
-        {/* <Stack.Screen name="(auth)" options={{ headerShown: false }} /> */}
-        {/* <Stack.Screen name="(home)" options={{ headerShown: false }} /> */}
         {/* <Stack.Screen name="(profile)" options={{ headerShown: false }} /> */}
       </Stack>
     </PaperProvider>
