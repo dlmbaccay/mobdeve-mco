7239e64cd589229ecc71eebc13b12970435648d2
Dominic Baccay <91103278+dlmbaccay@users.noreply.github.com>
2024-10-23 16:25:47 +0800

    added helper text within home to aid new users

--- components/topBar.tsx
+++ components/topBar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
-import { Card, Searchbar, Avatar, useTheme, List, Divider, ActivityIndicator } from 'react-native-paper';
+import { Card, Searchbar, Avatar, useTheme, List, Divider, ActivityIndicator, Chip } from 'react-native-paper';
 import { router } from "expo-router";
 import { User } from "../types/interfaces";
 
@@ -10,6 +10,7 @@ interface TopBarProps {
 }
 
 const TopBar = ({ user, handleSignOut }: TopBarProps) => {
+  const [showHelper, setShowHelper] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
   const [isCardOpen, setCardOpen] = useState(false);
   const theme = useTheme();
@@ -17,8 +18,8 @@ const TopBar = ({ user, handleSignOut }: TopBarProps) => {
   // TODO: Add a search functionality
 
   return (
-    <>
-      <Card className={`absolute top-0 w-[90%] h-fit my-6 p-2 rounded-3xl ${isCardOpen ? 'pb-0' : ''}`} style={{ backgroundColor: theme.colors.primaryContainer }}>
+    <View className={`absolute top-0 w-[90%]`}>
+      <Card className={`h-fit mt-6 mb-4 p-2 rounded-3xl ${isCardOpen ? 'pb-0' : ''}`} style={{ backgroundColor: theme.colors.primaryContainer }}>
         <Card.Content className="flex w-full items-center justify-between p-0 m-0">
           <View className="flex flex-row">
             <Searchbar
@@ -48,10 +49,20 @@ const TopBar = ({ user, handleSignOut }: TopBarProps) => {
               <List.Item title="Sign Out"  titleStyle={{ fontSize: 14 }} left={() => <List.Icon icon="exit-to-app" />} className="px-4" onPress={handleSignOut} />
             </List.Section>
           )}
-
         </Card.Content>
       </Card>
-    </>
+
+      { showHelper && (
+        <Chip
+          icon="information"
+          closeIcon="close"
+          onClose={() => setShowHelper(false)}
+          className="rounded-3xl w-fit"
+          mode="outlined"
+          textStyle={{ fontSize: 12 }}
+          >Long press on the map to report a condition!</Chip>
+      )}
+    </View>
   );
 };
 
